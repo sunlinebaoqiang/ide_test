50eaa2f506df7965f51e92cefe3717e51aac5bb2
sunlinebaoqiang <1689936115@qq.com>
2017-12-13 16:16:33 +0800

    提交

---
+++ src/test/java/com/helloworld/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package com.helloworld;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+	 @Test  
+	    public void testSayHello(){  
+	        HelloWorld helloWorld=new HelloWorld();  
+	        String result=helloWorld.sayHello();  
+	        assertEquals("Hello Maven",result);  
+	    } 
+
+}
